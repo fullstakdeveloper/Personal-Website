148a66cebdea1ad20fa43114d8fe4fb5a3286e91
fullstakdeveloper <pupru.edu2k7@gmail.com>
2024-04-17 12:42:19 -0400

    fixed some design concerns

--- personalwebsite/src/comps/project.js
+++ personalwebsite/src/comps/project.js
@@ -3,20 +3,7 @@ import './project.css';
 
 function Project() {
     return (
-        <div className = 'projects'>
-            <div className = 'title'>Projects</div>
-
-            <div className = 'pages'>
-
-                <div id = 'one2'>
-                    CCC Problem Solutions
-                </div>
-
-                <div id = 'two2'>
-                    Protivity-Productivity tool
-                </div>
-                
-            </div>
+        <div className = 'project'>
 
         </div>
     );
