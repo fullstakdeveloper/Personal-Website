dfe95c68fa880f2bcf935cf744e0025399767a04
fullstakdeveloper <pupru.edu2k7@gmail.com>
2024-04-20 12:39:46 -0400

    added some border radius

--- personalwebsite/src/App.js
+++ personalwebsite/src/App.js
@@ -7,25 +7,24 @@ import Project from './comps/project';
 
 function App() {
 
-  const intro_text = "Jashan is a curious and motivated individual who has developed programming skills as a result of his childhood love for Lego building. With a sharp attention to detail and a natural talent for problem-solving, he has expertly transformed simple bricks into intricate structures, applying coding principles to bring his creations to life. Jashan's journey into programming began when his father enrolled him in a web development course. What started as a simple step soon turned into an exhilarating adventure, as he discovered the endless possibilities that coding could unlock. Jashan's exploration continued to include the dynamic languages of Python and JavaScript. With each line of code, he found the artistry in simplicity, the logic in complexity, and the limitless potential of React's component-based architecture. Join him on this captivating journey, where every click and scroll reveals a story of growth, mastery, and the boundless horizons of programming prowess."
 
   return (
     <div className="App" >
 
       <motion.div className='name'
         animate = {{scale: 2, y: 25}}
-        transition={{ type: "spring", stiffness: 100, duration: 2, delay: 0.6}}
+        transition={{ stiffness: 100, duration: 1, delay: 0.6}}
       >
-        <div id='one'>
-          This is
-        </div>
+        <div id='one'>This is</div>
+        <div id='two'>Jashan Singh.</div>
 
-        <motion.div id='two'>Jashan Singh.</motion.div>
       </motion.div>
+
+
 
       <div className='intro'>
         <div id='one1'>
-          <div>{intro_text}</div>
+          <div>Jashan is a curious and motivated individual who has developed programming skills as a result of his childhood love for Lego building. With a sharp attention to detail and a natural talent for problem-solving, he has expertly transformed simple bricks into intricate structures, applying coding principles to bring his creations to life. Jashan's journey into programming began when his father enrolled him in a web development course. What started as a simple step soon turned into an exhilarating adventure, as he discovered the endless possibilities that coding could unlock. Jashan's exploration continued to include the dynamic languages of Python and JavaScript. With each line of code, he found the artistry in simplicity, the logic in complexity, and the limitless potential of React's component-based architecture. Join him on this captivating journey, where every click and scroll reveals a story of growth, mastery, and the boundless horizons of programming. Jashan is a curious and motivated individual who has developed programming skills as a result of his childhood love for Lego building. With a sharp attention to detail and a natural talent for problem-solving, he has expertly transformed simple bricks into intricate structures, applying coding principles to bring his creations to life. Jashan's journey into programming began when his father enrolled him in a web development course. What started as a simple step soon turned into an exhilarating adventure, as he discovered the endless possibilities that coding could unlock. Jashan's exploration continued to include the dynamic languages of Python and JavaScript. With each line of code, he found the artistry in simplicity, the logic in complexity, and the limitless potential of React's component-based architecture. Join him on this captivating journey, where every click and scroll reveals a story of growth, mastery, and the boundless horizons of programming. asdfsdfasdfasdfasdf</div>
 
           <div id="links">
             <a href='https://www.linkedin.com/in/jashan-singh-48b9592a0/' target = "_blank">Linked In</a>
@@ -35,6 +34,9 @@ function App() {
         </div>
 
         <div id='two1'><img src={PXL_20230731_183533541}></img></div>
+
+
+        
       </div>
 
 
