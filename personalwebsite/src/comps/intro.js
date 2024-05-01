f34bfc6157827fd1272e4d5ae97909f4c3402e24
fullstakdeveloper <pupru.edu2k7@gmail.com>
2024-04-30 23:03:07 -0400

    added some animations

--- personalwebsite/src/comps/intro.js
+++ personalwebsite/src/comps/intro.js
@@ -1,6 +1,6 @@
 import PXL_20230731_183533541 from './PXL_20230731_183533541.jpg';
 import React from 'react';
-import { motion } from 'framer-motion';
+import { motion, useScroll } from 'framer-motion';
 import './intro.css';
 
 
@@ -13,6 +13,8 @@ function Intro() {
       <motion.div className='name'
         animate = {{scale: 2}}
         transition={{ stiffness: 100, duration: 1, delay: 0.6}}
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1, delay: 0.5, duration: 3}}
       >
         <div id='one'>This is</div>
         <div id='two'>Jashan Singh.</div>
@@ -20,8 +22,15 @@ function Intro() {
       </motion.div>
 
 
-      <h1 className = "title3">About Him</h1>  
-      <div className='intro'>
+      <motion.h1 className = "title3"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1, delay: 0.5, duration: 3}}
+      >About Him</motion.h1>  
+
+      <motion.div className='intro'
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1, delay: 2, duration: 5}}
+      >
         <div id='one1'>
           <div>Jashan is a curious and motivated individual who has developed programming skills as a result of his childhood love for Lego building. With a sharp attention to detail and a natural talent for problem-solving, he has expertly transformed simple bricks into intricate structures, applying coding principles to bring his creations to life. Jashan's journey into programming began when his father enrolled him in a web development course. What started as a simple step soon turned into an exhilarating adventure, as he discovered the endless possibilities that coding could unlock. Jashan's exploration continued to include the dynamic languages of Python and JavaScript. With each line of code, he found the artistry in simplicity, the logic in complexity, and the limitless potential of React's component-based architecture. Join him on this captivating journey, where every click and scroll reveals a story of growth, mastery, and the boundless horizons of programming.</div>
 
@@ -42,7 +51,7 @@ function Intro() {
 
 
         
-      </div>
+      </motion.div>
 
     </div>
   );
