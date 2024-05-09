815eca1ff633e6650fff626d2c9d6d67fce37b36
fullstakdeveloper <pupru.edu2k7@gmail.com>
2024-05-09 14:07:05 -0400

    I added some more stuff

--- personalwebsite/src/comps/exp.js
+++ personalwebsite/src/comps/exp.js
@@ -5,14 +5,16 @@ function Exp() {
     return (
         <div className = 'exp'>
             <h1 className = "title4">Experiences</h1>
-            <div className = "box1">
-                
 
+            <div className = "box1">
+                <h1>Jamhacks 6</h1>
+                <p>yap about it</p>
             </div>
 
 
             <div className = 'box2'>
-
+                <h1>Clock Hacks 2</h1>
+                <p>Yap about it</p>
 
             </div>
         </div>
